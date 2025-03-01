8f7a626ae0e9f524c161e7a2785e5b5700bf76a1
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-02 00:01:40 +0530

    added onboarding popup

---
+++ src/components/Onboarding.jsx
@@ -0,0 +1,23 @@
+const Onboarding = () => {
+    return (
+        <div>
+            <div className="bg-[grey] opacity-70 h-screen w-screen flex items-center justify-center">
+                <div className=" border flex h-[500px] w-[900px] bg-white rounded-[1rem]">
+                    <div className="left w-[60%]">
+                        sdfsfsdfsdf
+                    </div>
+                    <div className="right w-[40%] flex flex-col py-[1rem] px-[1.5rem] justify-around">
+                        <h1>Lets Play Globetrotter </h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam inventore tempora officia ex ut ipsum ratione distinctio enim quaerat! Delectus voluptates voluptas ipsam ducimus expedita exercitationem architecto inventore commodi et.</p>
+                        <input type="text" placeholder="Enter Your name" className="border h-[3.5rem] rounded-[0.4rem] " />
+                        <button className="!bg-[#8000FF] text-white ">Submit</button>
+                        <a className="text-center">Skip</a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Onboarding
