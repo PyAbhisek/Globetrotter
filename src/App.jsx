01a747125790e511deefabdcd078ae36719c9c0e
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-02 10:36:55 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import Onboarding from './components/Onboarding'
 import Game from './components/Game'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
  
 
   return (
     <>
-    {/* <Onboarding/> */}
-    <Game/>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Onboarding />} /> */}
+        <Route path="/play" element={<Game />} />
+      </Routes>
+    </Router>
     </>
   )
 }
