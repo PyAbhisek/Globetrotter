f259276aa0360d10064511aba6df036b46210004
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-02 14:14:21 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate('/play');  // Redirect to /play on first load
-  }, []);
-
-  return null; // This ensures App itself doesn't render anything
+  return <Navigate to="/play" replace />;
 }
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Onboarding from './components/Onboarding';
 import Game from './components/Game';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/play" element={<Game />} />
-
+      <Route path="/onboard" element={<Onboarding />} />
     </Routes>
   </Router>
 );
